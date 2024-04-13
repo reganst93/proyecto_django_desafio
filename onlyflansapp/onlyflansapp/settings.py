c69fe55b21e190044089bb0c090231b2f86066ca
reganst93 <stmi.93@gmail.com>
2024-04-13 11:30:38 -0400

    correciones

--- onlyflansapp/onlyflansapp/settings.py
+++ onlyflansapp/onlyflansapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "static_pages.app.WebConfig",
+    "static_pages.apps.StaticPagesConfig",
 ]
 
 MIDDLEWARE = [
